c92b3443ed30106626e4ad250771792ec291eb4b
Alejandro <armentaayalaalejandro@gmail.com>
2026-02-28 16:14:03 -0600

    continued resnet

--- resnet.py
+++ resnet.py
@@ -55,5 +55,39 @@ class ResidualBlock(nn.Module):
 
 
 class ResNet(nn.Module):
-    pass
 
+    def make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+
+        if stride != 1 or self.inplanes != planes:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes,planes,kernel_size=1,stride=stride),
+                nn.BatchNorm2d(planes)
+            )
+
+        layers = []
+        
+        layers.append(
+            block(in_channels=self.inplanes, out_channels=planes, stride=stride, downsample=downsample)
+        )
+        
+        for i in range(1, blocks):
+            layers.append(
+                block(in_channels=self.inplanes, out_channels=planes)
+            )
+        
+        return nn.Sequential(*layers)
+
+    def __init__(self, block, layers, num_classes=10):
+        super().__init__()
+        
+        self.inplanes = 64
+        self.make_layer(block=block, planes=64, blocks=layers[0])
+        #self.make_layer(layers[1])
+        #self.make_layer(layers[2])
+        #self.make_layer(layers[3])
+        
+        nn.Linear(in_features=512,out_features=num_classes)
+
+
+ResNet(ResidualBlock, [3,4,6,3])
