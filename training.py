39ea094cd896910bf5ee3c79a589893392a3e1ce
Alejandro <armentaayalaalejandro@gmail.com>
2026-03-01 15:00:21 -0600

    fixed bug

--- training.py
+++ training.py
@@ -21,6 +21,9 @@ import gc
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+#del images, labels, outputs
+#del model
+#del optimizer
 torch.cuda.empty_cache()
 gc.collect()
 
@@ -80,7 +83,7 @@ model = ResNet(ResidualBlock, [3,4,6,3]).to(device=device)
 
 criterion = nn.CrossEntropyLoss()
 
-num_epochs = 1
+num_epochs = 20
 learnin_rate = 0.01
 
 #ay un weight decay en sgd
@@ -88,11 +91,12 @@ optimizer = torch.optim.SGD(model.parameters(), lr=learnin_rate, weight_decay=0.
 
 print(len(train_loader) * num_epochs)
 
-
+best_accuracy = 0
 
 for epoch in range(num_epochs):
 
-    loader = tqdm(train_loader)
+    loader = tqdm(train_loader, desc='training epoch')
+    
     
     for images, labels in loader:
 
@@ -108,12 +112,55 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
 
+        loader.set_postfix(loss=f"{loss.item():.4f}")
+
         del images, labels, pred
-        torch.cuda.empty_cache()
         gc.collect()
+        torch.cuda.empty_cache()
+    
+
+    print ('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, loss.item()))
+    
+
+    with torch.no_grad():
         
+        correct = 0
+        total = 0
 
+        loader = tqdm(valid_loader, desc='valid epoch')
 
+        for images, labels in loader:
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs = model(images)
+
+            #print(outputs.shape)
+
+            _, predicted = torch.max(outputs.data, 1)
+            
+            #print("predicted shape", predicted.shape)
+            #print("labels shape", labels.shape)
+
+            total += labels.size(0)
+            
+            correct += (predicted == labels).sum().item()
+
+            del images, labels, outputs
+            gc.collect()
+            torch.cuda.empty_cache()
+        
+        accuracy = 100 * correct / total
+        print('Accuracy of the network on the {} validation images: {} %'.format(5000, accuracy))
+
+        if accuracy > best_accuracy:
+            best_accuracy = accuracy
+            torch.save({
+                'epoch' : epoch,
+                'model_state_dict' : model.state_dict(),
+                'optimizer_state_dict' : optimizer.state_dict(),
+                'accuracy':accuracy
+            }, f='best_model.pt')
 
 
         
